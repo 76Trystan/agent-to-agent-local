529676557431029686f24f6525380e3765b8c35a
76Trystan <trystan.frederick@gmail.com>
2025-12-03 14:54:29 +1100

    updated handoffs

--- openai_agent.py
+++ openai_agent.py
@@ -1,6 +1,8 @@
 import asyncio
-from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunContextWrapper, Handoff
 from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+
+current_model = "llama3.1:8b"
 
 
 # create an AsyncOpenAI client that points to Ollama and provides a dummy key
@@ -9,14 +11,15 @@ ollama_client = AsyncOpenAI(
     api_key="ollama"   # OPenAI client requires an api_key even if Ollama does not
 )
 
+
 #---------------------------- specialist agents ----------------------------
 
-math_tutor_agent = Agent(
+math_agent = Agent(
     name="Math Tutor",
     handoff_description="Specialist agent for math questions",
-    instructions=f"""{RECOMMENDED_PROMPT_PREFIX}"You provide help with math problems, Explain your reasoning at each step and include examples.""",
+    instructions="You provide help with math problems, Explain your reasoning at each step and include examples.",
     model=OpenAIChatCompletionsModel(
-        model="llama3.1:8b",
+        model=current_model,
         openai_client=ollama_client,
     ),
 )
@@ -24,9 +27,9 @@ math_tutor_agent = Agent(
 poet_agent = Agent(
     name="Poet",
     handoff_description="Specialist in writing shakespearean poems",
-    instructions=f"""{RECOMMENDED_PROMPT_PREFIX}"You write shakespearean poems. based on the user's input, write in the style of shakespeare as if you were a poet from that era.""",
+    instructions="You write shakespearean poems. based on the user's input, write in the style of shakespeare as if you were a poet from that era.",
     model=OpenAIChatCompletionsModel(
-        model="llama3.1:8b",
+        model=current_model,
         openai_client=ollama_client,
     ),
 )
@@ -36,16 +39,21 @@ poet_agent = Agent(
 triage_agent = Agent(
     name="Triage Agent",
     instructions=f"""{RECOMMENDED_PROMPT_PREFIX}"You determine which agent to use based on the user's prompt. and provide which choice you made.""",
-    handoffs=[math_tutor_agent, poet_agent],
+    handoffs=[Handoff(agent=math_agent, condition="contains math"), Handoff(agent=poet_agent, condition="contains a word that you could write a poem about")],
     model=OpenAIChatCompletionsModel(
-        model="llama3.1:8b",
+        model=current_model,
         openai_client=ollama_client,
     )
 )
+#---------------------------- main program ----------------------------
 
 async def main():
-    result = await Runner.run(triage_agent, "whats 6 times 7 plus 2?")
+    result = await Runner.run(triage_agent, "whats 5 plus 3?")
     print(result.final_output)
+
+
+def on_handoff(_: RunContextWrapper[None]):
+    print("Handoff called: ", Agent.name)
 
 if __name__ == "__main__":
     asyncio.run(main())
