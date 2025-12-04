50485e6d23cb0aa3f0388ea88d2c7abb96844868
76Trystan <trystan.frederick@gmail.com>
2025-12-04 16:40:34 +1100

    changed prompt

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from agents import Runner
 from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
 
 async def main():
-    result = await Runner.run(openai_agent.triage_agent, "whats sydeney's weather like?")
+    result = await Runner.run(openai_agent.triage_agent, "write a poem about a blue table",)
     print(result.final_output)
 
 
