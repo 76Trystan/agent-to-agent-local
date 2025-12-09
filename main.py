a27438d7211ab7137255d600e07026367dffd473
76Trystan <trystan.frederick@gmail.com>
2025-12-09 14:02:23 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import agents.agent as agent
 
 
+
 def ask_agent(query: str):
     result = agent.triage_agent.invoke({"messages": [{"role": "user", "content": query}]})
     return result['messages'][-1].content
